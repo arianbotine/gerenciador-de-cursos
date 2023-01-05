203fedd631b8e057fdd841a59eed3762470308fa
Arian Botine <arian.botine@shift.com.br>
2023-01-05 20:42:17 -0300

    Implementando classe aulas e inciando testes para estudo

---
+++ br.com.treinamento/src/br/com/treinamento/TestaListaDeAula.java
@@ -0,0 +1,9 @@
+package br.com.treinamento;
+
+public class TestaListaDeAula {
+	public static void main(String[] args) {
+		Aula aula1 = new Aula("Reordenando listas",15);
+		Aula aula2 = new Aula("Aprendendo sobre lambdas",20);
+		Aula aula3 = new Aula("Testando listas",25);
+	}
+}
